bf58ef64c7a9d4688a28064078c2a0e50eb761b4
Weddly <weddly@hotmail.com>
2023-06-24 17:12:02 -0300

    Angular Tutorial Basic Project Implementation

---
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Hero } from 'src/app/interfaces/hero';
+import { HeroService } from 'src/app/services/hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: [ './hero-detail.component.css' ]
+})
+export class HeroDetailComponent implements OnInit {
+  selectedHero: Hero | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.selectedHero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    if (this.selectedHero) {
+      this.heroService.updateHero(this.selectedHero)
+        .subscribe(() => this.goBack());
+    }
+  }
+}
---
+++ src/assets/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "src/app/interfaces/hero";
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Cyclops' },
+  { id: 13, name: 'Beast' },
+  { id: 14, name: 'Iceman' },
+  { id: 15, name: 'Angel' },
+  { id: 16, name: 'Jean Grey' },
+  { id: 17, name: 'Banshee' },
+  { id: 18, name: 'Sunfire' },
+  { id: 19, name: 'Polaris' },
+  { id: 20, name: 'Havok' }
+];
